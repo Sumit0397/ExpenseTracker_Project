679a75e7e811f11fe39cc437b5419b3e79ad6dfc
Sumit0397 <sumitmajumdar16@gmail.com>
2023-08-28 18:02:48 +0530

    integrating with firebase

--- src/components/ExpensePage/ExpenseList.js
+++ src/components/ExpensePage/ExpenseList.js
@@ -1,16 +1,18 @@
 import React, { useContext } from 'react'
-import ExpenseContext from '../../store/expense-context'
+import ExpenseContext from '../../store/expense-context';
+import classes from "./ExpenseList.module.css";
 
 const ExpenseList = () => {
     const expCtx = useContext(ExpenseContext);
 
   return (
-    <div>
+    <div className={classes.listCon}>
+      <h1>Expenses</h1>
       <ul>
       {
-        expCtx.items.map((item) => (
-            <li >
-                <div>{item.date}</div>
+        expCtx.items.map((item , index) => (
+            <li className={classes.listItem} key={index}>
+                <div className={classes.date}>{item.date}</div>
                 <h3>{item.category.toUpperCase()}</h3>
                 <div>{item.description}</div>
                 <div>{item.amount}</div>
--- src/store/expense-context.js
+++ src/store/expense-context.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import AuthContext from "./auth-context";
 
 const ExpenseContext = React.createContext({
     items : [],
@@ -8,6 +10,29 @@ const ExpenseContext = React.createContext({
 
 export const ExpenseContextProvider = (props) => {
     const [itemsArr , setItemsArr] = useState([]);
+    const authCtx = useContext(AuthContext);
+
+    const restoreItems = async () => {
+        const email = authCtx.userEmail.replace(/[\.@]/g , "");
+        try {
+            const res = await axios.get(`https://expense-tracker-1fcaf-default-rtdb.firebaseio.com/${email}/expenses.json`)
+
+            const data = res.data;
+
+            const realData = Object.values(data).reverse();
+            setItemsArr(realData);
+        } catch (error) {
+            alert(error)
+        }
+    }
+
+    useEffect(() => {
+        restoreItems();
+    },[])
+
+    useEffect(() => {
+        restoreItems();
+    },[authCtx.userEmail])
 
     const addItemHandler = (item) => {
         setItemsArr([...itemsArr , item])
